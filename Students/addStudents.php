59975feaf9e25f7d887cd112bd7fcd1e7c6291b9
VIGNESH V <67644755+1041970@users.noreply.github.com>
2023-01-21 22:25:36 +0530

    Update addStudents.php
    here i have modified some values in columns

--- Students/addStudents.php
+++ Students/addStudents.php
@@ -2,9 +2,9 @@
 include('./config.inc');
 $responseCode='100';
 $responseMessage='Add Students Failure';
-$addStudentsQuery="INSERT INTO tblStudents(studentName, studentRollNo, studentDob, studentAddress,createdOn) VALUES ('Jagan','2108','2002-09-27','4/44 selva street',NOW()),
- ('Prakash','2108','2002-09-27','4/44 selva street',NOW()),
- ('MOhan','2108','2002-09-27','4/44 selva street',NOW())";
+$addStudentsQuery="INSERT INTO tblStudents(studentName, studentRollNo, studentDob, studentAddress,createdOn) VALUES ('Jagan','2107','2003-01-31','4/45 richie street',NOW()),
+ ('Prakash','2109','2002-11-02','4/46 ganesh street',NOW()),
+ ('MOhan','2110','2002-08-11','4/47 tirupathistreet',NOW())";
 $rsd = mysqli_query($connect_var, $addStudentsQuery);
 if($rsd)
 {
